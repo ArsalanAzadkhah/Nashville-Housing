3df26e4e8a7cdd482399a5b4a4d124d715845eca
ArsalanTheAnalyst <157727014+Amirarsalan006@users.noreply.github.com>
2024-03-24 13:44:26 -0400

    Update Full_Data_Cleanig_NashvilleHousing.sql

--- Full_Data_Cleanig_NashvilleHousing.sql
+++ Full_Data_Cleanig_NashvilleHousing.sql
@@ -1,30 +1,30 @@
--- drop table if exists housing
--- create table housing 
--- (
--- 	UniqueID serial primary key,
--- 	ParcelID varchar(100) ,
--- 	LandUse varchar(50) ,
--- 	PropertyAddress varchar(200) ,
--- 	SaleDate date ,
--- 	SalePrice bigint,
--- 	LegalReference varchar(200),
--- 	SoldAsVacant varchar(5),
--- 	OwnerName varchar(200) ,
--- 	OwnerAddress varchar(200) ,
--- 	Acreage numeric ,
--- 	TaxDistrict varchar(200) ,
--- 	LandValue numeric ,
--- 	BuildingValue numeric ,
--- 	TotalValue numeric ,
--- 	YearBuilt numeric ,
--- 	Bedrooms int ,
--- 	FullBath int ,
--- 	HalfBath int
--- )
+DROP TABLE IF EXISTS housing
+CREATE TABLE housing 
+(
+	UniqueID serial PRIMARY KEY,
+	ParcelID varchar(100) ,
+	LandUse varchar(50) ,
+	PropertyAddress varchar(200) ,
+	SaleDate date ,
+	SalePrice bigint,
+	LegalReference varchar(200),
+	SoldAsVacant varchar(5),
+	OwnerName varchar(200) ,
+	OwnerAddress varchar(200) ,
+	Acreage numeric ,
+	TaxDistrict varchar(200) ,
+	LandValue numeric ,
+	BuildingValue numeric ,
+	TotalValue numeric ,
+	YearBuilt numeric ,
+	Bedrooms int ,
+	FullBath int ,
+	HalfBath int
+)
 
 -- now, we import the data into the table and check if it's been imported
 
---select * from housing
+SELECT * FROM housing
 
 -- Next step is data cleaning :
 
@@ -32,66 +32,67 @@
 
  							--Checking Null Values
 							
--- select a.uniqueid,a.parcelid,a.propertyaddress,a.uniqueid,
--- 	   b.parcelid,b.propertyaddress 
--- from housing as a
--- join housing as b
--- 				on a.parcelid = b.parcelid
--- 				and	a.uniqueid <> b.uniqueid
--- 		where a.propertyaddress is null
+SELECT a.uniqueid,a.parcelid,a.propertyaddress,a.uniqueid,
+	   b.parcelid,b.propertyaddress 
+FROM housing AS a
+JOIN housing AS b
+		ON a.parcelid = b.parcelid
+		AND	a.uniqueid <> b.uniqueid
+		WHERE a.propertyaddress IS NULL
 
-							--Updating and cleaning null values
+					--Updating and cleaning null values
 							
--- UPDATE housing AS a
--- SET propertyaddress = COALESCE(a.propertyaddress, b.propertyaddress)
--- FROM housing AS b
--- WHERE a.parcelid = b.parcelid
---     AND a.uniqueid <> b.uniqueid
---     AND a.propertyaddress IS NULL;
-							-- Breaking down address into indivual columns
-							
--- ALTER TABLE housing 
--- ADD COLUMN	property_address varchar(50)
--- 	UPDATE housing
--- 	set property_address = SUBSTRING( propertyaddress, 1, POSITION(',' in propertyaddress)-1 )
+UPDATE housing AS a
+SET propertyaddress = COALESCE(a.propertyaddress, b.propertyaddress)
+FROM housing AS b
+WHERE a.parcelid = b.parcelid
+    AND a.uniqueid <> b.uniqueid
+    AND a.propertyaddress IS NULL;
 
--- ALTER TABLE housing
--- ADD COLUMN property_city varchar(50);
--- 	UPDATE housing
--- 	SET property_city = SUBSTRING( propertyaddress, POSITION(',' in propertyaddress)+2 )
+					-- Breaking down address into indivual columns
 
+--Breaking down Propertyaddress column into address and city
+ALTER TABLE housing 
+ADD COLUMN	property_address varchar(50)
+	UPDATE housing
+	SET property_address = SUBSTRING( propertyaddress, 1, POSITION(',' in propertyaddress)-1 )
 
+ALTER TABLE housing
+ADD COLUMN property_city varchar(50);
+	UPDATE housing
+	SET property_city = SUBSTRING( propertyaddress, POSITION(',' in propertyaddress)+2 )
 
--- ALTER TABLE housing 
--- ADD COLUMN	owner_address varchar(50)
--- 	UPDATE housing
--- 	SET owner_address = SPLIT_PART(owneraddress,',',1)
+--Breaking down OwnerAddress column into address, city, and state.
+ALTER TABLE housing 
+ADD COLUMN	owner_address varchar(50)
+	UPDATE housing
+	SET owner_address = SPLIT_PART(owneraddress,',',1)
 
--- ALTER TABLE housing 
--- ADD COLUMN	owner_city varchar(50)
--- 	UPDATE housing
--- 	SET owner_city = SPLIT_PART(owneraddress,',',2)
+ALTER TABLE housing 
+ADD COLUMN	owner_city varchar(50)
+	UPDATE housing
+	SET owner_city = SPLIT_PART(owneraddress,',',2)
 	
--- ALTER TABLE housing 
--- ADD COLUMN	owner_state varchar(50)
--- 	UPDATE housing
--- 	SET owner_state = SPLIT_PART(owneraddress,',',3)
+ALTER TABLE housing 
+ADD COLUMN	owner_state varchar(50)
+	UPDATE housing
+	SET owner_state = SPLIT_PART(owneraddress,',',3)
 
---select distinct(soldasvacant) from housing
+			-- Update missing values
 
--- update housing
--- set soldasvacant =  	
--- 	case soldasvacant
--- 		when 'Y' then 'Yes'
--- 		when 'N' then 'No'
--- 		when 'Yes' then 'Yes'
--- 		else 'No'
--- 	END
+UPDATE housing
+SET soldasvacant =  	
+	CASE soldasvacant
+		WHEN 'Y' THEN 'Yes'
+		WHEN 'N' THEN 'No'
+		WHEN 'Yes' THEN 'Yes'
+		ELSE 'No'
+	END
 
                         --Remove Unused Columns
 						
--- ALTER TABLE housing
--- DROP COLUMN propertyaddress, 
--- DROP COLUMN owneraddress
+ALTER TABLE housing
+	DROP COLUMN propertyaddress, 
+	DROP COLUMN owneraddress
 
-select * from housing
+SELECT * FROM housing
